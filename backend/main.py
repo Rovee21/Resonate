db4a2906d300eca308f0f8235a5c63c543ba1da7
Rovee21 <ro.vashisht21@gmail.com>
2026-01-10 19:32:43 -0500

    backend path changes

--- backend/main.py
+++ backend/main.py
@@ -110,7 +110,7 @@ def callback(code: str):
 
         # Redirect back to frontend with user info in query params
         frontend_url = (
-            f"{FRONTEND_URL}/profile"
+            f"{FRONTEND_URL}/WelcomePage"
             f"?name={user_profile.get('display_name', '')}"
             f"&image={image_url}"
             f"&access_token={access_token}"
